d88b5d42b8a49b0d6f3562079debec866bc00ff4
BronkenHit <142316635+BronkenHit@users.noreply.github.com>
2023-09-01 19:13:45 -0300

    Add files via upload

---
+++ explorer-projeto9/main.js
@@ -0,0 +1,63 @@
+//  1
+console.log("Hello World!");
+
+//  2
+let valueOne = 5;
+let valueTwo = 10;
+
+console.log(valueOne + valueTwo);
+
+// 3
+let variable = " ";
+
+if (typeof variable == 'string') {
+    console.log("Não é um número!");
+} else {
+    console.log("É um número!");
+}
+
+// 4
+let string = " ";
+
+if (typeof teste == 'string') {
+    console.log("É uma string!");
+} else {
+    console.log("Não é uma string!");
+}
+
+// 5
+let booleano = true;
+
+if (typeof variable == 'boolean') {
+    console.log("É um booleano!");
+} else {
+    console.log("Não é um booleano!");
+}
+
+// 6
+let numberOne = 5;
+let numberTwo = 10;
+
+console.log(numberOne - numberTwo);
+
+// 7
+let number1 = 5;
+let number2 = 10;
+
+console.log(number1 * number2);
+
+// 8
+let value1 = 5;
+let value2 = 10;
+
+console.log(value1 * value2);
+
+// 9
+let vrl = 5;
+let res = vrl % 2 == 0 ? "É um número par": "Não é um número par";
+console.log(res);
+
+// 10
+let vrl1 = 5;
+let res1 = vrl1 % 2 == 0 ? "É um número impar": "Não é um número impar";
+console.log(res1);
